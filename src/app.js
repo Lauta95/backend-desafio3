56fa0edd591ef7ae95f97f30b937b6a1ca2572d7
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-06-15 22:07:32 -0300

    req query

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import express from 'express'
 
 const app = express()
 
-app.get('/saludos', (req, res) => {
-    response.send('hola...')
+app.get('/saludo', (req, res) => {
+    console.log(req.query);
+    res.send('hola...')
 })
 
 app.listen(8080, () => console.log('escuchando 8080'))
